c9047dfa21c65e7b24807f6c9a914ac6fee6291d
Pierre Cauchois <pierreca@microsoft.com>
2016-11-14 18:46:30 -0800

    Fix build scripts for new repository

--- jenkins/linux_node.sh
+++ jenkins/linux_node.sh
@@ -3,7 +3,7 @@
 # Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 build_root=$(cd "$(dirname "$0")/.." && pwd)
-cd $build_root/node
+cd $build_root
 
 # Set up links in the npm cache to ensure we're exercising all the code in
 # the repo, rather than downloading released versions of our packages from
