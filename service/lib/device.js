dc2f2e1e1f70cdaad6c2b659953b058b3f3bd9c1
Tamer Awad <tameraw@microsoft.com>
2016-11-30 11:37:01 -0800

    Revert uninteded merge changes.

--- service/lib/device.js
+++ service/lib/device.js
@@ -6,6 +6,7 @@
 var _ = require('lodash');
 
 /**
+ * @deprecated
  * @class           module:azure-iothub.Device
  * @classdesc       Creates a representation of a device for use in the
  *                  identity {@link module:azure-iothub.Registry} APIs.
@@ -37,6 +38,8 @@ var _ = require('lodash');
  * @prop {Object}   authentication  Contains the symmetric keys used to
  *                                  authenticate this device
  */
+
+/*Codes_SRS_NODE_SERVICE_DEVICE_16_001: [The constructor shall accept a `null` or `undefined` value as argument and create an empty `Device` object.]*/
 module.exports = function Device(jsonData) {
   this.deviceId = null;
   this.generationId = null;
@@ -59,9 +62,12 @@ module.exports = function Device(jsonData) {
     }
   };
 
+  /*Codes_SRS_NODE_SERVICE_DEVICE_16_002: [If the `deviceDescription` argument is provided as a string, it shall be parsed as JSON and the properties of the new `Device` object shall be populated with the values provided in the `deviceDescription` JSON string.]*/
+  /*Codes_SRS_NODE_SERVICE_DEVICE_16_003: [If the `deviceDescription` argument if provided as an object, the properties of the new `Device` object shall be populated with the values provided in the `deviceDescription` JSON string.]*/
   if(jsonData) {
     var userProps = (typeof jsonData === 'string') ? JSON.parse(jsonData) : jsonData;
     if (!userProps.deviceId) {
+      /*Codes_SRS_NODE_SERVICE_DEVICE_16_004: [The constructor shall throw a `ReferenceError` if the `deviceDescription` argument doesn't contain a `deviceId` property.]*/
       throw new ReferenceError('The \'deviceId\' property cannot be \'' + userProps.deviceId + '\'');
     }
 
@@ -71,7 +77,8 @@ module.exports = function Device(jsonData) {
   Object.defineProperty(this.authentication, 'SymmetricKey', {
     enumerable: true,
     get: function () {
-      return this.authentication ? this.authentication.symmetricKey : undefined;
+      /*Codes_SRS_NODE_SERVICE_DEVICE_16_005: [The `authentication.SymmetricKey` property shall return the content of the `authentication.symmetricKey` property (the latter being the valid property returned by the IoT hub in the device description).]*/
+      return this.symmetricKey;
     }
   });
 };
