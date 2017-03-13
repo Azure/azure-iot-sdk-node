a8ee848e00a5ba91c2b55046bb9f01b14daa9fca
Pierre Cauchois <pierreca@microsoft.com>
2017-03-13 16:00:32 -0700

    Remove dev-teardown from jenkins build steps on linux (using containers - not necessary anymore)

--- jenkins/linux_node.sh
+++ jenkins/linux_node.sh
@@ -18,10 +18,4 @@ build/dev-setup.sh
 build/build.sh --min --integration-tests --e2e-tests
 [ $? -eq 0 ] || exit $?
 
-# The 'npm link' commands in this script create symlinks to tracked repo
-# files from ignored locations (under ./node_modules). This means a call to
-# 'git clean -xdf' will delete tracked files from the repo's working
-# directory. To avoid any complications, we'll unlink everything before
-# exiting.
-build/dev-teardown.sh
-[ $? -eq 0 ] || exit $?
+# No need to run dev-teardown.sh anymore since we're running of containers
