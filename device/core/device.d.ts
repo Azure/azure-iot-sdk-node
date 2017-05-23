2c3b7a5787a38e6ed70bb362fc6e4ff9444b3b74
Thomas Huber <thomas3577@users.noreply.github.com>
2017-05-23 16:43:06 +0200

    wrong path
    It seems to be the wrong path

--- device/core/device.d.ts
+++ device/core/device.d.ts
@@ -6,4 +6,4 @@ export import ConnectionString = require('./lib/connection_string');
 export import SharedAccessSignature = require('./lib/shared_access_signature');
 export { StableConnectionTransport, TwinTransport, BatchingTransport, ClientConfig } from './lib/interfaces';
 export { Message } from 'azure-iot-common';
-export { DeviceMethodRequest, DeviceMethodResponse } from './src/device_method';
+export { DeviceMethodRequest, DeviceMethodResponse } from './lib/device_method';
