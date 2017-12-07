805fea098576063751311d7bdaf62eab8a759c78
Pierre Cauchois <pierreca@microsoft.com>
2017-12-06 23:55:32 -0800

    [Provisioning] AMQP Transport and tests

--- provisioning/transport/amqp/devdoc/amqp_requirements.md
+++ provisioning/transport/amqp/devdoc/amqp_requirements.md
@@ -1,98 +1,95 @@
 # Amqp Requirements
 
-The `Amqp` class provides the Provisioning Device SDK with a transport for TPM and x509 registration flows over AMQP.
-
-# Example usage
-
-```js
-var Amqp = require('azure-iot-provisioning-device-amqp').Amqp;
-var ProvisioningDeviceClient = require('azure-iot-provisioning-device').Client;
-var X509Security = require('azure-iot-device-security-x509').X509Security;
-
-var transport = new Amqp(idScope, dpsHost);
-var securityClient = new X509Security(deviceCert);
-var deviceClient = ProvisioningDeviceClient.create(transport, securityClient);
-
-[...]
-```
-
-# Public API
-
-This is the public API surface that the customer will use.
-
-## constructor
-
-The `Amqp` constructor shall throw a `ReferenceError` if the `scopeId` argument is falsy.
-
-The `Amqp` constructor shall throw a `ReferenceError` if the `provisioningServiceEndpoint` argument is falsy.
-
+The `Amqp` class provides the Provisioning Device SDK with a transport for TPM and x509 registration flows over AMQP. It is instantiated by SDK users only to be passed to the `ProvisioningDeviceClient` object and does not have a public API.
 
 # Internal API
 
-These methods are considered public from a Typescript perspective but private from a public API surface perspective because the customers shouldn't use these directly.
+These methods are used by the other objects of the SDK but are not public API for the SDK user to call.
 
-## registerX509(registrationId: string, auth: X509, forceRegistration: boolean, callback: (err?: Error, registrationResult?: X509RegistrationResult, body?: any, result?: any) => void): void
+ ## setAuthentication(auth: X509): void
 
-The `registerX509` method shall connect the AMQP client with the certificate and key given in the `auth` parameter.
+**SRS_NODE_PROVISIONING_AMQP_16_001: [** The certificate and key passed as properties of the `auth` argument shall be used to connect to the Device Provisioning Service endpoint, when a registration request or registration operation status request are made. **]**
 
-The `registerX509` method shall attach a sender link on the `<idScope>/registrations/<registrationId>` endpoint with the following properties:
+## registrationRequest(request: RegistrationRequest, callback: (err?: Error, responseBody?: any, result?: any, pollingInterval?: number) => void): void
+
+**SRS_NODE_PROVISIONING_AMQP_16_002: [** The `registrationRequest` method shall connect the AMQP client with the certificate and key given in the `auth` parameter of the previously called `setAuthentication` method. **]**
+
+**SRS_NODE_PROVISIONING_AMQP_16_003: [** The `registrationRequest` method shall attach a sender link on the `<idScope>/registrations/<registrationId>` endpoint with the following properties:
 ```
 com.microsoft:api-version: <API_VERSION>
 com.microsoft:client-version: <CLIENT_VERSION>
 ```
+**]**
 
-The `registerX509` method shall attach a receiver link on the `<idScope>/registrations/<registrationId>` endpoint with the following properties:
+**SRS_NODE_PROVISIONING_AMQP_16_004: [** The `registrationRequest` method shall attach a receiver link on the `<idScope>/registrations/<registrationId>` endpoint with the following properties:
 ```
 com.microsoft:api-version: <API_VERSION>
 com.microsoft:client-version: <CLIENT_VERSION>
 ```
+**]**
 
-The `registerX509` method shall use a polling state machine to trigger the registration process.
-
-## registrationRequest
-
-The `registrationRequest` method shall send a message on the pre-attached sender link with a `correlationId` set to a newly generated UUID and the following application properties:
+**SRS_NODE_PROVISIONING_AMQP_16_005: [** The `registrationRequest` method shall send a message on the previously attached sender link with a `correlationId` set to a newly generated UUID and the following application properties:
 ```
 iotdps-operation-type: iotdps-register;
 iotdps-forceRegistration: <true or false>;
 ```
+ **]**
 
-The `registrationRequest` method shall listen for the response on the receiver link and accept it when it comes.
+**SRS_NODE_PROVISIONING_AMQP_16_006: [** The `registrationRequest` method shall listen for the response on the receiver link and accept it when it comes. **]**
 
-The `registrationRequest` method shall call its callback with a `InitialRegistrationResponse` object with the following properties populated from the response message:
+**SRS_NODE_PROVISIONING_AMQP_16_007: [** The `registrationRequest` method shall call its callback with a `RegistrationResult` object parsed from the body of the response message which `correlationId` matches the `correlationId` of the request message sent on the sender link. **]**
+
+**SRS_NODE_PROVISIONING_AMQP_16_008: [** The `registrationRequest` method shall call its callback with an error if the transport fails to connect. **]**
+
+**SRS_NODE_PROVISIONING_AMQP_16_009: [** The `registrationRequest` method shall call its callback with an error if the transport fails to attach the sender link. **]**
+
+**SRS_NODE_PROVISIONING_AMQP_16_010: [** The `registrationRequest` method shall call its callback with an error if the transport fails to attach the receiver link. **]**
+
+**SRS_NODE_PROVISIONING_AMQP_16_011: [** The `registrationRequest` method shall call its callback with an error if the transport fails to send the request message. **]**
+
+## queryOperationStatus(request: RegistrationRequest, operationId: string, callback: (err?: Error, responseBody?: any, result?: any, pollingInterval?: number) => void): void
+
+**SRS_NODE_PROVISIONING_AMQP_16_012: [** The `queryOperationStatus` method shall connect the AMQP client with the certificate and key given in the `auth` parameter of the previously called `setAuthentication` method. **]** **]**
+
+**SRS_NODE_PROVISIONING_AMQP_16_013: [** The `queryOperationStatus` method shall attach a sender link on the `<idScope>/registrations/<registrationId>` endpoint with the following properties:
 ```
-TBD
+com.microsoft:api-version: <API_VERSION>
+com.microsoft:client-version: <CLIENT_VERSION>
 ```
+**]**
 
+**SRS_NODE_PROVISIONING_AMQP_16_014: [** The `queryOperationStatus` method shall attach a receiver link on the `<idScope>/registrations/<registrationId>` endpoint with the following properties:
+```
+com.microsoft:api-version: <API_VERSION>
+com.microsoft:client-version: <CLIENT_VERSION>
+```
+**]**
 
-## operationStatusRequest
-
-The `operationStatusRequest` method shall send a message on the pre-attached sender link with a `correlationId` set to a newly generated UUID and the following application properties:
+**SRS_NODE_PROVISIONING_AMQP_16_015: [** The `queryOperationStatus` method shall send a message on the pre-attached sender link with a `correlationId` set to a newly generated UUID and the following application properties:
 ```
 iotdps-operation-type: iotdps-get-operationstatus;
 iotdps-operation-id: <operationId>;
 ```
+**]**
 
-The `registrationRequest` method shall listen for the response on the receiver link and accept it when it comes.
+**SRS_NODE_PROVISIONING_AMQP_16_016: [** The `queryOperationStatus` method shall listen for the response on the receiver link and accept it when it comes. **]**
 
-The `registrationRequest` method shall call its callback with a `OperationStatusResponse` object with the following properties populated from the response message:
-```
-TBD
-```
+**SRS_NODE_PROVISIONING_AMQP_16_017: [** The `queryOperationStatus` method shall call its callback with a `RegistrationResult` object parsed from the body of the response message which `correlationId` matches the `correlationId` of the request message sent on the sender link. **]**
 
-## cancel(callback: (err: Error) => void): void
+**SRS_NODE_PROVISIONING_AMQP_16_018: [** The `queryOperationStatus` method shall call its callback with an error if the transport fails to connect. **]**
 
-`cancel` shall call its callback immediately if the AMQP connection is disconnected.
+**SRS_NODE_PROVISIONING_AMQP_16_019: [** The `queryOperationStatus` method shall call its callback with an error if the transport fails to attach the sender link. **]**
 
-`cancel` shall call `cancel` on the polling state machine if it is running.
+**SRS_NODE_PROVISIONING_AMQP_16_020: [** The `queryOperationStatus` method shall call its callback with an error if the transport fails to attach the receiver link. **]**
 
-`cancel` shall detach the sender and receiver links and disconnect the AMQP connection.
+**SRS_NODE_PROVISIONING_AMQP_16_021: [** The `queryOperationStatus` method shall call its callback with an error if the transport fails to send the request message. **]**
 
-`cancel` shall call its callback with no arguments if all detach/disconnect operations were successful
+## cancel(callback: (err?: Error) => void): void
 
-`cancel` shall call its callback with the error passed from the first unsuccessful detach/disconnect operation if one of those fail.
+**SRS_NODE_PROVISIONING_AMQP_16_022: [** `cancel` shall call its callback immediately if the AMQP connection is disconnected. **]**
 
-## getAuthenticationChallenge(registrationInfo: TpmRegistrationInfo, callback: (err: Error, tpmChallenge?: TpmChallenge) => void): void
+**SRS_NODE_PROVISIONING_AMQP_16_023: [** `cancel` shall detach the sender and receiver links and disconnect the AMQP connection. **]**
 
-## register(registrationInfo: TpmRegistrationInfo, sasToken: string, callback: (err: Error, result?: TpmRegistrationResult) => void): void
+**SRS_NODE_PROVISIONING_AMQP_16_024: [** `cancel` shall call its callback with no arguments if all detach/disconnect operations were successful. **]**
 
+**SRS_NODE_PROVISIONING_AMQP_16_025: [** `cancel` shall call its callback with the error passed from the first unsuccessful detach/disconnect operation if one of those fail. **]**
--- provisioning/transport/amqp/src/amqp.ts
+++ provisioning/transport/amqp/src/amqp.ts
@@ -14,27 +14,22 @@ const debug = dbg('azure-device-provisioning-amqp:Amqp');
 // TODO: use apiVersion and userAgent from constants.ts
 import { X509 } from 'azure-iot-common';
 import { ProvisioningTransportOptions, X509ProvisioningTransport, RegistrationRequest, RegistrationResult, ProvisioningDeviceConstants } from 'azure-iot-provisioning-device';
-import { TpmProvisioningTransport, TpmRegistrationInfo, TpmChallenge } from 'azure-iot-provisioning-device';
 import { Amqp as Base, SenderLink, ReceiverLink, AmqpMessage } from 'azure-iot-amqp-base';
 
-class MessagePropertyNames {
-    // tslint:disable:variable-name
-    static OperationType: string = 'iotdps-operation-type';
-    static OperationId: string = 'iotdps-operation-id';
-    static Status: string = 'iotdps-status';
-    static ForceRegistration: string = 'iotdps-forceRegistration';
-    // tslint:enable:variable-name
+enum MessagePropertyNames {
+    OperationType = 'iotdps-operation-type',
+    OperationId = 'iotdps-operation-id',
+    Status = 'iotdps-status',
+    ForceRegistration = 'iotdps-forceRegistration'
 }
 
-class DeviceOperations {
-    // tslint:disable:variable-name
-    static Register: string = 'iotdps-register';
-    static GetRegistration: string = 'iotdps-get-registration';
-    static GetOperationStatus: string = 'iotdps-get-operationstatus';
-    // tslint:enable:variable-name
+enum DeviceOperations {
+    Register = 'iotdps-register',
+    GetRegistration = 'iotdps-get-registration',
+    GetOperationStatus = 'iotdps-get-operationstatus'
 }
 
-export class Amqp extends EventEmitter implements X509ProvisioningTransport, TpmProvisioningTransport  {
+export class Amqp extends EventEmitter implements X509ProvisioningTransport {
   private _amqpBase: Base;
   private _config: ProvisioningTransportOptions = {};
   private _amqpStateMachine: machina.Fsm;
@@ -45,7 +40,7 @@ export class Amqp extends EventEmitter implements X509ProvisioningTransport, Tpm
   private _senderLink: SenderLink;
 
   private _operations: {
-    [key: string]: (err: Error, result?: RegistrationResult) => void;
+    [key: string]: (err?: Error, result?: RegistrationResult, transportResponse?: AmqpMessage, pollingInterval?: number) => void;
   } = {};
 
   constructor(amqpBase?: Base) {
@@ -58,9 +53,11 @@ export class Amqp extends EventEmitter implements X509ProvisioningTransport, Tpm
 
     const responseHandler = (msg) => {
       debug('got message with correlationId: ' + msg.correlationId);
+      /*Codes_SRS_NODE_PROVISIONING_AMQP_16_007: [The `registrationRequest` method shall call its callback with a `RegistrationResult` object parsed from the body of the response message which `correlationId` matches the `correlationId` of the request message sent on the sender link.]*/
+      /*Codes_SRS_NODE_PROVISIONING_AMQP_16_017: [The `queryOperationStatus` method shall call its callback with a `RegistrationResult` object parsed from the body of the response message which `correlationId` matches the `correlationId` of the request message sent on the sender link.]*/
       const registrationResult = JSON.parse(msg.data);
       if (this._operations[msg.correlationId]) {
-        this._operations[msg.correlationId](null, registrationResult);
+        this._operations[msg.correlationId](null, registrationResult, msg, this._config.pollingInterval);
       } else {
         debug('ignoring message with unknown correlationId');
       }
@@ -78,41 +75,39 @@ export class Amqp extends EventEmitter implements X509ProvisioningTransport, Tpm
               this.emit('error', err);
             }
           },
-          getAuthenticationChallenge: () => {
-            // TODO
-          },
-          registrationRequest: (request, requestBody, callback) => {
+          registrationRequest: (request, callback) => {
             // TODO: this only works for x509
             this._amqpStateMachine.transition('connecting', request, (err) => {
-              if (!err) {
-                this._amqpStateMachine.handle('registrationRequest', request, requestBody, callback);
+              if (err) {
+                callback(err);
+              } else {
+                this._amqpStateMachine.handle('registrationRequest', request, callback);
               }
             });
           },
           queryOperationStatus: (request, operationId, callback) => {
             // TODO: this only works for x509
             this._amqpStateMachine.transition('connecting', request, (err) => {
-              if (!err) {
+              if (err) {
+                callback(err);
+              } else {
                 this._amqpStateMachine.handle('queryOperationStatus', request, operationId, callback);
               }
             });
           },
-          sendMessage: (request, requestMessage, callback) => {
-            // TODO: this only works for x509
-            this._amqpStateMachine.transition('connecting', request, (err) => {
-              if (!err) {
-                this._amqpStateMachine.handle('sendMessage', request, requestMessage, callback);
-              }
-            });
-          },
+          /*Codes_SRS_NODE_PROVISIONING_AMQP_16_022: [`cancel` shall call its callback immediately if the AMQP connection is disconnected.]*/
           cancel: (callback) => callback()
         },
         connecting: {
           _onEnter: (request, callback) => {
+            /*Codes_SRS_NODE_PROVISIONING_AMQP_16_002: [The `registrationRequest` method shall connect the AMQP client with the certificate and key given in the `auth` parameter of the previously called `setAuthentication` method.]*/
+            /*Codes_SRS_NODE_PROVISIONING_AMQP_16_012: [The `queryOperationStatus` method shall connect the AMQP client with the certificate and key given in the `auth` parameter of the previously called `setAuthentication` method. **]**]*/
             this._amqpBase.connect('amqps://' + request.provisioningHost, this._x509Auth, (err) => {
               if (err) {
                 debug('_amqpBase.connect failed');
                 debug(err);
+                /*Codes_SRS_NODE_PROVISIONING_AMQP_16_008: [The `registrationRequest` method shall call its callback with an error if the transport fails to connect.]*/
+                /*Codes_SRS_NODE_PROVISIONING_AMQP_16_018: [The `queryOperationStatus` method shall call its callback with an error if the transport fails to connect.]*/
                 this._amqpStateMachine.transition('disconnected', err, null, callback);
               } else {
                 const linkEndpoint = request.idScope + '/registrations/' + request.registrationId;
@@ -125,19 +120,46 @@ export class Amqp extends EventEmitter implements X509ProvisioningTransport, Tpm
                   }
                 };
 
+                /*Codes_SRS_NODE_PROVISIONING_AMQP_16_004: [The `registrationRequest` method shall attach a receiver link on the `<idScope>/registrations/<registrationId>` endpoint with the following properties:
+                ```
+                com.microsoft:api-version: <API_VERSION>
+                com.microsoft:client-version: <CLIENT_VERSION>
+                ```]*/
+                /*Codes_SRS_NODE_PROVISIONING_AMQP_16_014: [The `queryOperationStatus` method shall attach a receiver link on the `<idScope>/registrations/<registrationId>` endpoint with the following properties:
+                ```
+                com.microsoft:api-version: <API_VERSION>
+                com.microsoft:client-version: <CLIENT_VERSION>
+                ```*/
                 this._amqpBase.attachReceiverLink(linkEndpoint, linkOptions, (err, receiverLink) => {
                   if (err) {
                     debug('_amqpBase.attachReceiverLink failed');
                     debug(err);
+                    /*Codes_SRS_NODE_PROVISIONING_AMQP_16_010: [The `registrationRequest` method shall call its callback with an error if the transport fails to attach the receiver link.]*/
+                    /*Codes_SRS_NODE_PROVISIONING_AMQP_16_020: [The `queryOperationStatus` method shall call its callback with an error if the transport fails to attach the receiver link.]*/
                     this._amqpStateMachine.transition('disconnecting', err, null, callback);
                   } else {
                     this._receiverLink = receiverLink;
                     this._receiverLink.on('error', amqpErrorListener);
+                    /*Codes_SRS_NODE_PROVISIONING_AMQP_16_006: [The `registrationRequest` method shall listen for the response on the receiver link and accept it when it comes.]*/
+                    /*Codes_SRS_NODE_PROVISIONING_AMQP_16_016: [The `queryOperationStatus` method shall listen for the response on the receiver link and accept it when it comes.]*/
                     this._receiverLink.on('message', responseHandler);
+
+                    /*Codes_SRS_NODE_PROVISIONING_AMQP_16_003: [The `registrationRequest` method shall attach a sender link on the `<idScope>/registrations/<registrationId>` endpoint with the following properties:
+                    ```
+                    com.microsoft:api-version: <API_VERSION>
+                    com.microsoft:client-version: <CLIENT_VERSION>
+                    ```]*/
+                    /*Codes_SRS_NODE_PROVISIONING_AMQP_16_013: [The `queryOperationStatus` method shall attach a sender link on the `<idScope>/registrations/<registrationId>` endpoint with the following properties:
+                    ```
+                    com.microsoft:api-version: <API_VERSION>
+                    com.microsoft:client-version: <CLIENT_VERSION>
+                    ```*/
                     this._amqpBase.attachSenderLink(linkEndpoint, linkOptions, (err, senderLink) => {
                       if (err) {
                         debug('_amqpBase.attachSenderLink failed');
                         debug(err);
+                        /*Codes_SRS_NODE_PROVISIONING_AMQP_16_009: [The `registrationRequest` method shall call its callback with an error if the transport fails to attach the sender link.]*/
+                        /*Codes_SRS_NODE_PROVISIONING_AMQP_16_019: [The `queryOperationStatus` method shall call its callback with an error if the transport fails to attach the sender link.]*/
                         this._amqpStateMachine.transition('disconnecting', err, null, callback);
                       } else {
                         this._senderLink = senderLink;
@@ -150,12 +172,20 @@ export class Amqp extends EventEmitter implements X509ProvisioningTransport, Tpm
               }
             });
           },
+          /*Codes_SRS_NODE_PROVISIONING_AMQP_16_023: [`cancel` shall detach the sender and receiver links and disconnect the AMQP connection.]*/
           cancel: (callback) => this._amqpStateMachine.transition('disconnecting', null, null, callback),
           '*': () => this._amqpStateMachine.deferUntilTransition()
         },
         connected: {
           _onEnter: (callback) => callback(),
-          registrationRequest: (request, requestBody, callback) => {
+          registrationRequest: (request, callback) => {
+
+            /*Codes_SRS_NODE_PROVISIONING_AMQP_16_005: [The `registrationRequest` method shall send a message on the previously attached sender link with a `correlationId` set to a newly generated UUID and the following application properties:
+            ```
+            iotdps-operation-type: iotdps-register;
+            iotdps-forceRegistration: <true or false>;
+            ```
+            ]*/
             let requestMessage = new AmqpMessage();
             requestMessage.body = '';
             requestMessage.applicationProperties = {};
@@ -165,10 +195,23 @@ export class Amqp extends EventEmitter implements X509ProvisioningTransport, Tpm
             requestMessage.properties.correlationId = uuid.v4();
 
             debug('initial registration request: ' + JSON.stringify(requestMessage));
-
-            this._amqpStateMachine.handle('sendMessage', request, requestMessage, callback);
+            this._operations[requestMessage.properties.correlationId] = callback;
+            this._senderLink.send(requestMessage, (err) => {
+              if (err) {
+                this._operations[requestMessage.properties.correlationId] = undefined;
+                /*Codes_SRS_NODE_PROVISIONING_AMQP_16_011: [The `registrationRequest` method shall call its callback with an error if the transport fails to send the request message.]*/
+                callback(err, null, null, this._config.pollingInterval);
+              } else {
+                debug('registration request sent with correlationId: ' + requestMessage.properties.correlationId);
+              }
+            });
           },
           queryOperationStatus: (request, operationId, callback) => {
+            /*Codes_SRS_NODE_PROVISIONING_AMQP_16_015: [The `queryOperationStatus` method shall send a message on the pre-attached sender link with a `correlationId` set to a newly generated UUID and the following application properties:
+            ```
+            iotdps-operation-type: iotdps-get-operationstatus;
+            iotdps-operation-id: <operationId>;
+            ```*/
             let requestMessage = new AmqpMessage();
             requestMessage.body = '';
             requestMessage.applicationProperties = {};
@@ -178,18 +221,14 @@ export class Amqp extends EventEmitter implements X509ProvisioningTransport, Tpm
             requestMessage.properties.correlationId = uuid.v4();
 
             debug('registration status request: ' + JSON.stringify(requestMessage));
-
-            this._amqpStateMachine.handle('sendMessage', request, requestMessage, callback);
-          },
-          sendMessage: (request, requestMessage, callback) => {
-            debug('saving requestMessage with correlationId: ' + requestMessage.properties.correlationId);
             this._operations[requestMessage.properties.correlationId] = callback;
-
             this._senderLink.send(requestMessage, (err) => {
               if (err) {
-                this._amqpStateMachine.transition('disconnecting', err, null, callback);
+                this._operations[requestMessage.properties.correlationId] = undefined;
+                /*Codes_SRS_NODE_PROVISIONING_AMQP_16_021: [The `queryOperationStatus` method shall call its callback with an error if the transport fails to send the request message.]*/
+                callback(err, null, null, this._config.pollingInterval);
               } else {
-                debug('registration request sent');
+                debug('registration status request sent with correlationId: ' + requestMessage.properties.correlationId);
               }
             });
           },
@@ -251,6 +290,8 @@ export class Amqp extends EventEmitter implements X509ProvisioningTransport, Tpm
                 });
               }
             ], () => {
+              /*Codes_SRS_NODE_PROVISIONING_AMQP_16_024: [`cancel` shall call its callback with no arguments if all detach/disconnect operations were successful.]*/
+              /*Codes_SRS_NODE_PROVISIONING_AMQP_16_025: [`cancel` shall call its callback with the error passed from the first unsuccessful detach/disconnect operation if one of those fail.]*/
               this._amqpStateMachine.transition('disconnected', finalError, registrationResult, callback);
             });
           },
@@ -273,27 +314,20 @@ export class Amqp extends EventEmitter implements X509ProvisioningTransport, Tpm
     });
   }
 
-  /* X509ProvisioningTransport implementation */
   setAuthentication(auth: X509): void {
+    /*Codes_SRS_NODE_PROVISIONING_AMQP_16_001: [The certificate and key passed as properties of the `auth` argument shall be used to connect to the Device Provisioning Service endpoint, when a registration request or registration operation status request are made.]*/
     this._x509Auth = auth;
   }
 
-  /* TpmProvisioningTransport implementation */
-  getAuthenticationChallenge(registrationInfo: TpmRegistrationInfo, callback: (err?: Error, tpmChallenge?: TpmChallenge) => void): void {
-    this._amqpStateMachine.handle('getAuthenticationChallenge', registrationInfo, callback);
-  }
-
-  cancel(callback: (err?: Error) => void): void {
-    this._amqpStateMachine.handle('cancel', callback);
-  }
-
-  /* PollingTransportHandler Implementation */
-  registrationRequest(request: RegistrationRequest, requestBody: any, callback: (err?: Error, responseBody?: any, result?: any, pollingInterval?: number) => void): void {
-    this._amqpStateMachine.handle('registrationRequest', request, requestBody, callback);
+  registrationRequest(request: RegistrationRequest, callback: (err?: Error, responseBody?: any, result?: any, pollingInterval?: number) => void): void {
+    this._amqpStateMachine.handle('registrationRequest', request, callback);
   }
 
   queryOperationStatus(request: RegistrationRequest, operationId: string, callback: (err?: Error, responseBody?: any, result?: any, pollingInterval?: number) => void): void {
     this._amqpStateMachine.handle('queryOperationStatus', request, operationId, callback);
   }
 
+  cancel(callback: (err?: Error) => void): void {
+    this._amqpStateMachine.handle('cancel', callback);
+  }
 }
