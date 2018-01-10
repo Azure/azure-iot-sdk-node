ea0a412184ecd8e5df3d8f0ec4f695a56ac609a7
Fabian Klein Schiphorst <fyboo@hotmail.com>
2018-01-10 13:06:43 +0100

    create createCertificate.js
    Helpful for creating x509 certificates for individual enrollments of device in device provisioning service. Is inline with example given in the device part of the provisioning documentation.

---
+++ provisioning/service/samples/createCertificate.js
@@ -0,0 +1,31 @@
+'use strict';
+var pem = require('pem');
+var fs = require('fs');
+var certPath = __dirname + "\\cert\\";
+
+var deviceID = process.argv[2].toLowerCase();
+var cert = certPath + deviceID + '-cert.pem';
+var key = certPath + deviceID + '-key.pem'
+
+var certOptions = {
+    commonName: deviceID,
+    selfSigned: true,
+    days: 10
+};
+
+// if certificate directory doesn't exists, create it
+if (!fs.existsSync(certPath)) {
+    console.log("Creating folder: " + certPath);
+    fs.mkdirSync(certPath);
+};
+
+//creating certificates
+pem.createCertificate(certOptions, function (err, result) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Creating certificate " + cert + " and key: " + key );
+        fs.writeFileSync(cert, result.certificate);
+        fs.writeFileSync(key, result.clientKey);
+    }
+});
