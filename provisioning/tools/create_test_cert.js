6149c3293e5fdeaf4abed11f12effc4c2d73e84b
Bert Kleewein <bertk@microsoft.com>
2018-03-15 14:42:24 -0700

    udpate usage of create_test_cert.js to be more intuitive for verification operations

--- provisioning/tools/create_test_cert.js
+++ provisioning/tools/create_test_cert.js
@@ -9,57 +9,60 @@ var chalk = require('chalk');
 var camelcase = require('camelcase');
 var argv = require('yargs')
           .command('root [commonName]', 'create a root certificate', (yargs) => {
-            yargs.positional('commonName', { describe: 'name to use for cert', default: 'Test Root Cert' })
+            return yargs
+              .positional('commonName', { describe: 'name to use for cert', default: 'Test Root Cert' })
           })
           .command('intermediate <commonName> <parentCommonName>', 'create an intermediate cert', (yargs) => {
-            yargs.positional('commonName', { describe: 'name to use for cert' })
-            yargs.positional('parentCommonName', { describe: 'common name of parent cert' })
+            return yargs
+              .positional('commonName', { describe: 'name to use for cert' })
+              .positional('parentCommonName', { describe: 'common name of parent cert' });
           })
           .command('device <commonName> [parentCommonName]', 'create a device cert', (yargs) => {
-            yargs.positional('commonName', { describe: 'name to use for cert' })
-            yargs.positional('parentCommonName', { describe: 'common name of parent cert' })
+            return yargs
+              .positional('commonName', { describe: 'name to use for cert' })
+              .positional('parentCommonName', { describe: 'common name of parent cert' });
           })
-          .command('verification <certToVerify> <verificationCode>', 'provide proof of possession for a cert using a verification code', (yargs) => {
-            yargs.positional('certToVerify', { describe: 'common name for certificate to verify' })
-            yargs.positional('verificationCode', { describe: 'verification code from portal' })
+          .command('verification [--ca root cert pem file name] [--key root cert key pem file name] [--nonce nonce]', 'provide proof of possession for a cert using a verification code', (yargs) => {
+            return yargs
+              .option('ca', { describe: 'pem file name of the root' })
+              .option('key', { describe: 'pem file name of the key' })
+              .option('nonce', { describe: 'verification code from portal' })
+              .demandOption(['ca', 'key', 'nonce']);
           })
           .demandCommand()
-          .help()
+          .help(false)
+          .version(false)
+          .wrap(null)
           .argv;
 
 var command = argv._[0];
 
 var commonName = null;
 var parentCommonName = null;
-var parentFilenameRoot = null;
-var certFilenameRoot = null;
 
-if (command === 'verification') {
-  commonName = argv.verificationCode;
-  certFilenameRoot = 'verification';
-
-  parentCommonName = argv.certToVerify;
-  parentFilenameRoot = camelcase(parentCommonName);
-} else {
-  commonName = argv.commonName;
-  certFilenameRoot = camelcase(commonName);
-
-  if (argv.parentCommonName) {
-    parentCommonName = argv.parentCommonName;
-    parentFilenameRoot = camelcase(parentCommonName);
-  }
-}
+var outputFilenameRoot = null;
 
 var parentCert = null;
 var parentKey = null;
 var parentChain = null;
-if (parentCommonName) {
-  console.log(chalk.green('reading parent cert from ' + parentFilenameRoot + '_*.pem'));
-  parentCert = fs.readFileSync(parentFilenameRoot + '_cert.pem').toString('ascii');
-  parentKey = fs.readFileSync(parentFilenameRoot + '_key.pem').toString('ascii');
-  parentChain = fs.readFileSync(parentFilenameRoot + '_fullchain.pem').toString('ascii');
+
+if (command === 'verification') {
+  commonName = argv.nonce;
+  outputFilenameRoot = 'verification';
+
+  parentCert = fs.readFileSync(argv.ca).toString('ascii');
+  parentKey = fs.readFileSync(argv.key).toString('ascii');
 } else {
-  parentChain = '';
+  commonName = argv.commonName;
+  outputFilenameRoot = camelcase(commonName);
+
+  if (argv.parentCommonName) {
+    var parentFilenameRoot = camelcase(argv.parentCommonName);
+    console.log(chalk.green('reading parent cert from ' + parentFilenameRoot + '_*.pem'));
+    parentCert = fs.readFileSync(parentFilenameRoot + '_cert.pem').toString('ascii');
+    parentKey = fs.readFileSync(parentFilenameRoot + '_key.pem').toString('ascii');
+    parentChain = fs.readFileSync(parentFilenameRoot + '_fullchain.pem').toString('ascii');
+  }
 }
 
 var certOptions = {
@@ -104,12 +107,12 @@ pem.createCertificate(certOptions, function(err, cert) {
     console.log(chalk.red('Could not create certificate: ' + err.message));
     process.exit(1);
   } else {
-    console.log(chalk.green('saving cert to ' + certFilenameRoot + '_cert.pem.'));
-    fs.writeFileSync(certFilenameRoot + '_cert.pem', cert.certificate);
-    console.log(chalk.green('saving key to ' + certFilenameRoot + '_key.pem.'));
-    fs.writeFileSync(certFilenameRoot + '_key.pem', cert.clientKey);
+    console.log(chalk.green('saving cert to ' + outputFilenameRoot + '_cert.pem.'));
+    fs.writeFileSync(outputFilenameRoot + '_cert.pem', cert.certificate);
+    console.log(chalk.green('saving key to ' + outputFilenameRoot + '_key.pem.'));
+    fs.writeFileSync(outputFilenameRoot + '_key.pem', cert.clientKey);
     // Note: this saves the root cert as part of the chain.  This isn't necessary, but it doesn't hurt either.
-    console.log(chalk.green('saving cert with chain to ' + certFilenameRoot + '_fullchain.pem.'));
-    fs.writeFileSync(certFilenameRoot + '_fullchain.pem', cert.certificate + '\n' + parentChain);
+    console.log(chalk.green('saving cert with chain to ' + outputFilenameRoot + '_fullchain.pem.'));
+    fs.writeFileSync(outputFilenameRoot + '_fullchain.pem', cert.certificate + '\n' + parentChain);
   }
 });
