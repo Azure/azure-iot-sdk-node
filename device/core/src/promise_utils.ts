3c10e7db5d0180d422c540b677ccb3567463bcd5
Kamil Pajdzik <kamip@microsoft.com>
2018-09-07 10:45:36 -0700

    Add callbackToPromise method

--- device/core/src/promise_utils.ts
+++ device/core/src/promise_utils.ts
@@ -2,27 +2,18 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 'use strict';
 
+export type NoErrorCallback<TResult> = (result?: TResult) => void;
 export type ErrorCallback = (error?: Error) => void;
 export type Callback<TResult> = (error?: Error, result?: TResult) => void;
 
-// export type Callback2<TResult1, TResult2> = (error?: Error, result1?: TResult1) => void;
+export function callbackToPromise<TResult>(callBackOperation: (callback: Callback<TResult>) => void): Promise<TResult> {
+    return new Promise((resolve, reject) => {
+      callBackOperation((error, result) => {
+        if (error) {
+          return reject(error);
+        }
 
-// export function getCallbackFromParameters<TParam, TResult>(param?: TParam, callback?: Callback<TResult>): Callback<TResult> | undefined {
-//     if (!callback && typeof param === "function") {
-//         callback = options;
-//         options = undefined;
-//       }
-// }
-
-// export function callbackToPromise<TResult>(operation: ((xxx, callback: Callback<TResult>) => void)): Promise<TResult> {
-//     return new Promise<TResult>((resolve, reject) => {
-//         operation((err, result) => {
-//             if (err) {
-//                 return reject(err);
-//             }
-    
-//             return resolve(result);
-//          });
-//     });
-// };
-
+        return resolve(result);
+      });
+    });
+  }
