805e47884d8c8db789e918f27b4c4d32b3f5ce3d
Kamil Pajdzik <kamip@microsoft.com>
2018-09-11 12:54:44 -0700

    Add helper methods for non standard callbacks

--- common/core/src/promise_utils.ts
+++ common/core/src/promise_utils.ts
@@ -2,18 +2,45 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 'use strict';
 
+export type MultiValueCallback<TResult1, TResult2> = (result1?: TResult1, result2?: TResult2) => void;
 export type NoErrorCallback<TResult> = (result?: TResult) => void;
 export type ErrorCallback = (error?: Error) => void;
-export type Callback<TResult> = (error?: Error, result?: TResult) => void;
+export type Callback<TResult> = MultiValueCallback<Error, TResult>;
 
 export function callbackToPromise<TResult>(callBackOperation: (callback: Callback<TResult>) => void): Promise<TResult> {
-    return new Promise((resolve, reject) => {
-      callBackOperation((error, result) => {
-        if (error) {
-          return reject(error);
-        }
+  return new Promise((resolve, reject) => {
+    callBackOperation((error, result) => {
+      if (error) {
+        return reject(error);
+      }
 
-        return resolve(result);
-      });
+      return resolve(result);
     });
-  }
+  });
+}
+
+export function noErrorCallbackToPromise<TResult>(callBackOperation: (callback: NoErrorCallback<TResult>) => void): Promise<TResult> {
+  return new Promise((resolve, _reject) => {
+    callBackOperation((result) => {
+      return resolve(result);
+    });
+  });
+}
+
+export function multiValueCallbackToPromise<TResult1, TResult2, TPromiseResult>(
+  callBackOperation: (callback: MultiValueCallback<TResult1, TResult2>) => void,
+  packResults: (result1: TResult1, result2: TResult2) => TPromiseResult): Promise<TPromiseResult> {
+  return new Promise((resolve, reject) => {
+    callBackOperation((result1, result2) => {
+      if (result1 instanceof Error) {
+        reject(result1);
+      }
+
+      if (result2 instanceof Error) {
+        reject(result2);
+      }
+
+      return resolve(packResults(result1, result2));
+    });
+  });
+}
