3e0c3f7ac931de6ed0b66994bffd4c18289e84bc
Kamil Pajdzik <kamip@microsoft.com>
2018-09-13 17:30:50 -0700

    Fix import

--- device/core/src/device_method/index.ts
+++ device/core/src/device_method/index.ts
@@ -1,9 +1,9 @@
-import { Callback } from 'azure-iot-common';
-
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 'use strict';
+
+import { Callback } from 'azure-iot-common';
 
 export { DeviceMethodRequest } from './device_method_request';
 export { DeviceMethodResponse } from './device_method_response';
