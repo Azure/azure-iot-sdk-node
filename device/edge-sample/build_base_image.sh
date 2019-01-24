85377b57e6550ea71eaa6fee372e7594a9f83c43
Pierre Cauchois <pierreca@microsoft.com>
2019-01-24 11:35:32 -0800

    fix(#426): update module samples to use fromEnvironment

--- device/edge-sample/build_base_image.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Copyright (c) Microsoft. All rights reserved.
-# Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-tar -czh . | docker build -t iot-hub-module-base:latest -f Dockerfile.base -
-exit $?
-
-
