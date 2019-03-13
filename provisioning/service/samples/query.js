6a084e0092e66e77dbdc1bbef128896c81681779
Pierre Cauchois <pierreca@microsoft.com>
2019-03-12 18:00:56 -0700

    Fix arg parsing in provisioning query sample

--- provisioning/service/samples/query.js
+++ provisioning/service/samples/query.js
@@ -6,8 +6,8 @@
 var provisioningServiceClient = require('azure-iot-provisioning-service').ProvisioningServiceClient;
 
 var argv = require('yargs')
-  .usage('Usage: $0 --connectionstring <DEVICE PROVISIONING CONNECTION STRING> ')
-  .option('connectionstring', {
+  .usage('Usage: $0 --connectionString <DEVICE PROVISIONING CONNECTION STRING> ')
+  .option('connectionString', {
     alias: 'c',
     describe: 'The connection string for the Device Provisioning instance',
     type: 'string',
