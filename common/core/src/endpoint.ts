7108d37e91eb2a3c5254c682407ac35be8940402
Anthony Ercolano <anthonyvercolano@outlook.com>
2019-05-06 14:20:29 -0700

    (fix) - Rollback IoT Hub client API string.

--- common/core/src/endpoint.ts
+++ common/core/src/endpoint.ts
@@ -4,7 +4,7 @@
 
 'use strict';
 
-export const apiVersion = '2019-03-30';
+export const apiVersion = '2018-06-30';
 
 export function devicePath(deviceId: string): string {
   return '/devices/' + deviceId;
