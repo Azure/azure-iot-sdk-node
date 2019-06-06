deb8e0886fb9d4dabec5063d248f6c564444a4b6
Yoseph Maguire <yoseph.maguire@microsoft.com>
2019-06-06 10:44:45 -0700

    fix Buffer Alloc Test

--- e2etests/test/device_service.js
+++ e2etests/test/device_service.js
@@ -195,7 +195,7 @@ function device_service_tests(deviceTransport, createDeviceMethod) {
       var uuidData = uuid.v4();
       var sizeOfNonPayload = (2*uuidData.length) + 1;
       var bufferSize = maximumMessageSize - sizeOfNonPayload;
-      var buffer = Buffer.from(bufferSize);
+      var buffer = Buffer.alloc(bufferSize);
       var message = new Message(buffer);
       debug(' message max send: size of non playload ' + sizeOfNonPayload);
       debug(' message max send: buffersize ' + bufferSize);
