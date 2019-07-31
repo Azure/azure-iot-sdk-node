e1b39e3a278b4ac33d85bd5872904b1948777e0e
Pierre Cauchois <pierreca@microsoft.com>
2019-07-31 13:52:41 -0700

    Fix query sample to use interface version

--- digitaltwins/samples/service/query_interface.js
+++ digitaltwins/samples/service/query_interface.js
@@ -5,12 +5,13 @@
 
 const Registry = require('azure-iothub').Registry;
 
-const interfaceId = '<INTERFACE_ID_GOES_HERE>'; // suggestion to find your environmental sensor: urn:contoso:com:EnvironmentalSensor:1
+const interfaceId = '<interface name>'; // suggestion to find your environmental sensor: 'urn:contoso:com:EnvironmentalSensor'
+const interfaceVersion = 1; // replace this with the version number you want. we suggest to start with 1 :);
 
 async function main() {
   const registryClient = Registry.fromConnectionString(process.env.IOTHUB_CONNECTION_STRING);
   console.log('querying devices that have interface ' + interfaceId + '...');
-  const query = registryClient.createQuery('SELECT * from devices  WHERE has_interface(\'' + interfaceId + '\')');
+  const query = registryClient.createQuery(`SELECT * from devices  WHERE has_interface('${interfaceId}', ${interfaceVersion})`);
   while (query.hasMoreResults) {
     const queryResponse = await query.next(query.continuationToken);
     queryResponse.result.forEach((device) => console.log(device.deviceId));
