f9744c1df9bdf1a194ca95a0d57c6b02f4ae8ba6
zolvarga <zolvarga@microsoft.com>
2019-10-28 11:04:16 -0700

    Fix  build error

--- digitaltwins/model_repository/index.js
+++ digitaltwins/model_repository/index.js
@@ -3,4 +3,6 @@
 module.exports = {
   ModelRepositoryCredentials: require('./dist/auth/model_repository_credentials').ModelRepositoryCredentials,
   ModelRepositoryServiceClient: require('./dist/cl/model_repository_service_client').ModelRepositoryServiceClient,
+  DigitalTwinRepositoryService: require('./dist/pl/digitalTwinRepositoryService').DigitalTwinRepositoryService,
+  DigitalTwinRepositoryServiceModels: require('./dist/pl/digitalTwinRepositoryService').DigitalTwinRepositoryServiceModels,
 };
