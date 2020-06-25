becfce94379b51ec5d3d8e1170a0af50db5d7d06
olivakar <oliva.kar@microsoft.com>
2020-06-25 12:02:04 -0700

    chore: node pnp sample send tele and process exit

--- digitaltwins/samples/device/javascript/pnpSampleDevice.js
+++ digitaltwins/samples/device/javascript/pnpSampleDevice.js
@@ -6,7 +6,6 @@
 const Protocol = require('azure-iot-device-mqtt').Mqtt;
 const Client = require('azure-iot-device').Client;
 const Message = require('azure-iot-device').Message;
-// const { promisify } = require('util')
 
 const packageJson = require('./package.json');
 
@@ -21,6 +20,7 @@ const deviceInfoComponentName = 'deviceInformation';
 const sdkInfoComponentName = 'sdkInformation';
 const componentPrefix = '$iotin:';
 const commandComponentCommandNameSeparator = '*';
+let intervalToken;
 
 const commandNameBlink = componentPrefix + sensorComponentName + commandComponentCommandNameSeparator + 'blink';
 const commandNameTurnOn = componentPrefix + sensorComponentName + commandComponentCommandNameSeparator + 'turnOn';
@@ -119,27 +119,42 @@ const helperAttachHandlerForDesiredPropertyPatches = (deviceTwin, componentName)
   });
 };
 
+const helperAttachExitListener = async (deviceClient) => {
+  const standardInput = process.stdin;
+  standardInput.setEncoding('utf-8');
 
-const sendTelemetryAtInterval = (deviceClient, componentName, secs) => {
-  let index = 0;
-  const interval = parseFloat(secs)*1000;
-  setInterval(() => {
-    console.log('Sending telemetry message %d...', index);
-    const data = JSON.stringify({ temp: 1 + (Math.random() * 90), humidity: 1 + (Math.random() * 99) });
-    const pnpMsg = new Message(data);
-    pnpMsg.properties.add(messageSubjectProperty, componentName);
-    pnpMsg.contentType = 'application/json';
-    pnpMsg.contentEncoding = 'utf-8';
-    deviceClient.sendEvent(pnpMsg);
-    index += 1;
-  }, interval);
+  // Prompt user to input data in console.
+  console.log('Please enter q or Q to exit sample.');
+  standardInput.on('data', (data) => {
+  // standard_input.on('data', async function (data) {
+    if (data === 'q\n' || data === 'Q\n') {
+      console.log('Clearing intervals and exiting sample.');
+      clearInterval(intervalToken);
+      // await deviceClient.close();
+      deviceClient.close();
+      process.exit();
+    } else {
+      console.log('User Input was : ' + data);
+      console.log('Please only enter q or Q to exit sample.');
+    }
+  });
 };
+async function sendTelemetry(deviceClient, componentName, index) {
+  console.log('Sending telemetry message %d...', index);
+  const data = JSON.stringify({ temp: 1 + (Math.random() * 90), humidity: 1 + (Math.random() * 99) });
+  const pnpMsg = new Message(data);
+  pnpMsg.properties.add(messageSubjectProperty, componentName);
+  pnpMsg.contentType = 'application/json';
+  pnpMsg.contentEncoding = 'utf-8';
+  await deviceClient.sendEvent(pnpMsg);
+}
 
 async function main() {
   // fromConnectionString must specify a transport, coming from any transport package.
   const client = Client.fromConnectionString(deviceConnectionString, Protocol);
   console.log('Connecting using connection string ' + deviceConnectionString);
   let resultTwin;
+
   try {
     // Add the modelId here
     await client.setOptions({ modelId: modelId });
@@ -151,7 +166,11 @@ async function main() {
     // Is there no way to register for an unknown method ?
 
     // Send Telemetry every 5.5 secs
-    await sendTelemetryAtInterval(client, sensorComponentName, 5.5);
+    let index = 0;
+    intervalToken = setInterval(() => {
+      sendTelemetry(client, sensorComponentName, index).catch((err) => console.log('error', err.toString()));
+      index += 1;
+    }, 5500);
 
     try {
       resultTwin = await client.getTwin();
@@ -183,11 +202,16 @@ async function main() {
       updateComponentReportedProperties(resultTwin, patchSDKInfo, sdkInfoComponentName);
       updateComponentReportedProperties(resultTwin, patchSensorInfo, sensorComponentName);
       helperAttachHandlerForDesiredPropertyPatches(resultTwin, sensorComponentName);
+
+      helperAttachExitListener(client);
     } catch (err) {
       console.error('could not retrieve twin or report twin properties\n' + err.toString());
     }
   } catch (err) {
     console.error('could not connect pnp client\n' + err.toString());
+  } finally {
+    console.log('entering and leaving the finally block');
+    // await client.close();
   }
 }
 
