d2b19d35b20565f907a5035745744b468cae5480
Jelani Brandon <jelanibrandon@gmail.com>
2020-07-02 11:05:03 -0700

    chore: updating test for mqtt unittest for model-id

--- device/transport/mqtt/test/_mqtt_test.js
+++ device/transport/mqtt/test/_mqtt_test.js
@@ -637,7 +637,7 @@ describe('Mqtt', function () {
     /* Tests_SRS_NODE_DEVICE_MQTT_41_014: [For a Plug and Play Device the modelId should be included as `&modelId=<DEVICE’s MODEL ID>` after the api-version]*/
     /* Tests_SRS_NODE_DEVICE_MQTT_41_015: [If a modelId is provided, the device should use the PnP API String] */
     it('sets options for modelId', function (testCallback) {
-      const fakeModelId = '&modelId=fakeModelId';
+      const fakeModelId = '&model-id=fakeModelId';
       const fakeModel = { modelId: 'fakeModelId' };
       let connectCallback;
       fakeMqttBase.connect = sinon.stub().callsFake(function (config, callback) {
