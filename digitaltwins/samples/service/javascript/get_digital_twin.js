d9b9a9f991720ff238787f1804c9659a3710fdb8
Jelani Brandon <jelanibrandon@gmail.com>
2020-07-14 13:31:59 -0700

    chore: fix sample to include credential

--- digitaltwins/samples/service/javascript/get_digital_twin.js
+++ digitaltwins/samples/service/javascript/get_digital_twin.js
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-// const IoTHubTokenCredentials = require('azure-iot-digitaltwins-service').IoTHubTokenCredentials;
-const digitalTwinServiceClient = require('azure-iot-digitaltwins-service').DigitalTwinServiceClient;
+const IoTHubTokenCredentials = require('azure-iot-digitaltwins-service').IoTHubTokenCredentials;
+const DigitalTwinServiceClient = require('azure-iot-digitaltwins-service').DigitalTwinServiceClient;
 const { inspect } = require('util');
 
 // Simple example of how to:
@@ -14,9 +14,14 @@ const { inspect } = require('util');
 // - Twin enabled device must exist on the ADT hub
 async function main() {
   const deviceId = process.env.IOTHUB_DEVICE_ID;
+  const connString = process.env.IOTHUB_CONNECTION_STRING;
+
+  // Create digital twin service client
+  const credentials = new IoTHubTokenCredentials(connString);
+  const dtServiceclient = new DigitalTwinServiceClient(credentials);
 
   // Get digital twin and retrieve the modelId from it
-  const digitalTwin = await digitalTwinServiceClient.getDigitalTwin(deviceId);
+  const digitalTwin = await dtServiceclient.getDigitalTwin(deviceId);
 
   console.log(inspect(digitalTwin));
   console.log('Model Id: ' + inspect(digitalTwin.$metadata.$model));
