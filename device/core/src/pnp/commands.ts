445d1e20ff0a9935a2e3ea4996124b6c04d42abb
Vishnu Reddy <vishnureddy@microsoft.com>
2021-08-13 08:46:34 -0700

    change indent to 2 spaces

--- device/core/src/pnp/commands.ts
+++ device/core/src/pnp/commands.ts
@@ -3,60 +3,60 @@ import { DeviceMethodRequest, DeviceMethodResponse } from '../device_method';
 import { ErrorCallback } from 'azure-iot-common';
 
 export class CommandRequest {
-    /**
-     * The request identifier supplied by the service for this command call.
-     */
-    requestId: string;
+  /**
+   * The request identifier supplied by the service for this command call.
+   */
+  requestId: string;
 
-    /**
-     * The name of the command being called.
-     */
-    commandName: string;
+  /**
+   * The name of the command being called.
+   */
+  commandName: string;
 
-    /**
-     * The name of the component corresponding to the command being called.
-     */
-    componentName?: string;
+  /**
+   * The name of the component corresponding to the command being called.
+   */
+  componentName?: string;
 
-    /**
-     * The payload of this command call.
-     */
-    payload: JSONSerializableValue;
+  /**
+   * The payload of this command call.
+   */
+  payload: JSONSerializableValue;
 
-    constructor(methodRequest: DeviceMethodRequest) {
-        this.requestId = methodRequest.requestId;
-        this.payload = methodRequest.payload;
-        const splitMethod = methodRequest.methodName.split('*');
-        if (splitMethod.length > 1) {
-            this.componentName = splitMethod[0];
-            this.commandName = splitMethod.slice(1).join('*');
-        } else {
-            this.commandName = splitMethod[0];
-        }
+  constructor(methodRequest: DeviceMethodRequest) {
+    this.requestId = methodRequest.requestId;
+    this.payload = methodRequest.payload;
+    const splitMethod = methodRequest.methodName.split('*');
+    if (splitMethod.length > 1) {
+      this.componentName = splitMethod[0];
+      this.commandName = splitMethod.slice(1).join('*');
+    } else {
+      this.commandName = splitMethod[0];
     }
+  }
 }
 
 export class CommandResponse {
-    private methodSend: (status: number, payload?: any | ErrorCallback, done?: ErrorCallback) => Promise<void> | void;
+  private methodSend: (status: number, payload?: any | ErrorCallback, done?: ErrorCallback) => Promise<void> | void;
 
-    constructor(methodResponse: DeviceMethodResponse) {
-        this.methodSend = methodResponse.send.bind(methodResponse);
-    }
+  constructor(methodResponse: DeviceMethodResponse) {
+    this.methodSend = methodResponse.send.bind(methodResponse);
+  }
 
-    /**
-     * Sends a response to the service for a command request.
-     *
-     * @param {number}                status   A numeric status code to be sent back to the service
-     * @param {JSONSerializableValue} payload  A json-serializable object representing the payload of command response.
-     * @param {function}              callback A callback function which will be called once the response has been sent to the service.
-     *                                  If an error occurs, an error object is passed as the first argument to the callback.
-     *                                  If the callback is not specified, send() will return a Promise.
-     */
-    send(status: number): Promise<void>;
-    send(status: number, payload: JSONSerializableValue): Promise<void>;
-    send(status: number, callback: ErrorCallback): void;
-    send(status: number, payload: JSONSerializableValue, callback: ErrorCallback): void;
-    send(status: number, payloadOrCallback?: JSONSerializableValue | ErrorCallback, callback?: ErrorCallback): Promise<void> | void {
-        return this.methodSend(status, payloadOrCallback, callback);
-    }
+  /**
+   * Sends a response to the service for a command request.
+   *
+   * @param {number}                status   A numeric status code to be sent back to the service
+   * @param {JSONSerializableValue} payload  A json-serializable object representing the payload of command response.
+   * @param {function}              callback A callback function which will be called once the response has been sent to the service.
+   *                                  If an error occurs, an error object is passed as the first argument to the callback.
+   *                                  If the callback is not specified, send() will return a Promise.
+   */
+  send(status: number): Promise<void>;
+  send(status: number, payload: JSONSerializableValue): Promise<void>;
+  send(status: number, callback: ErrorCallback): void;
+  send(status: number, payload: JSONSerializableValue, callback: ErrorCallback): void;
+  send(status: number, payloadOrCallback?: JSONSerializableValue | ErrorCallback, callback?: ErrorCallback): Promise<void> | void {
+    return this.methodSend(status, payloadOrCallback, callback);
+  }
 }
--- device/core/src/pnp/properties.ts
+++ device/core/src/pnp/properties.ts
@@ -2,82 +2,82 @@ import { JSONSerializableValue, JSONSerializableObject } from '.';
 import { Twin } from '../twin';
 
 export function generateWritablePropertyResponse(value: JSONSerializableValue, ackCode: number, ackDescription: string, ackVersion: number): {
-    value: JSONSerializableValue,
-    ac: number,
-    ad: string,
-    av: number
+  value: JSONSerializableValue,
+  ac: number,
+  ad: string,
+  av: number
 } {
-    return {
-        value,
-        ac: ackCode,
-        ad: ackDescription,
-        av: ackVersion
-    };
+  return {
+    value,
+    ac: ackCode,
+    ad: ackDescription,
+    av: ackVersion
+  };
 }
 
 export class ClientPropertyCollection {
-    /**
-     * The object representation of the properties.
-     */
-    backingObject: JSONSerializableObject;
+  /**
+   * The object representation of the properties.
+   */
+  backingObject: JSONSerializableObject;
 
-    constructor(backingObject?: JSONSerializableObject) {
-        this.backingObject = backingObject ?? {};
-    }
+  constructor(backingObject?: JSONSerializableObject) {
+    this.backingObject = backingObject ?? {};
+  }
 
-    /**
-     * The $version field of the properties.
-     * Returns NaN if the $version field is undefined or cannot be converted to a number.
-     */
-    get version(): number {
-        return Number(this.backingObject.$version);
-    }
+  /**
+   * The $version field of the properties.
+   * Returns NaN if the $version field is undefined or cannot be converted to a number.
+   */
+  get version(): number {
+    return Number(this.backingObject.$version);
+  }
 
-    /**
-     * Assigns the provided value to a specified property for a component.
-     * If no component is specified, the default component is assumed.
-     *
-     * @param {string}                componentName The name of the component.
-     * @param {string}                propertyName  The name of the property.
-     * @param {JSONSerializableValue} value         The value to assign to the property.
-     */
-    setProperty(propertyName: string, value: JSONSerializableValue): void;
-    setProperty(componentName: string, propertyName: string, value: JSONSerializableValue): void;
-    setProperty(propertyNameOrComponentName: string, valueOrPropertyName: JSONSerializableValue, value?: JSONSerializableValue): void {
-        if (value === undefined) { // We cannot just check the truthiness of value because null is a valid argument for value
-            this.backingObject[propertyNameOrComponentName] = valueOrPropertyName;
-        } else {
-            if (typeof this.backingObject[propertyNameOrComponentName] !== 'object') {
-                this.backingObject[propertyNameOrComponentName] = {}
-            }
-            this.backingObject[propertyNameOrComponentName]['__t'] = 'c'
-            this.backingObject[propertyNameOrComponentName][valueOrPropertyName as string] = value;
-        }
+  /**
+   * Assigns the provided value to a specified property for a component.
+   * If no component is specified, the default component is assumed.
+   *
+   * @param {string}                componentName The name of the component.
+   * @param {string}                propertyName  The name of the property.
+   * @param {JSONSerializableValue} value         The value to assign to the property.
+   */
+  setProperty(propertyName: string, value: JSONSerializableValue): void;
+  setProperty(componentName: string, propertyName: string, value: JSONSerializableValue): void;
+  setProperty(propertyNameOrComponentName: string, valueOrPropertyName: JSONSerializableValue, value?: JSONSerializableValue): void {
+    if (value === undefined) { // We cannot just check the truthiness of value because null is a valid argument for value
+      this.backingObject[propertyNameOrComponentName] = valueOrPropertyName;
+    } else {
+      if (typeof this.backingObject[propertyNameOrComponentName] !== 'object') {
+        this.backingObject[propertyNameOrComponentName] = {}
+      }
+      this.backingObject[propertyNameOrComponentName]['__t'] = 'c'
+      this.backingObject[propertyNameOrComponentName][valueOrPropertyName as string] = value;
     }
+  }
 
-    /**
-     * Gets the value assigned to the specified component and property.
-     * If no component is specified, the default component is assumed.
-     * Returns undefined if the property does not exist.
-     *
-     * @param {string} componentName The name of the component.
-     * @param {string} propertyName  The name of the property.
-     */
-    getProperty(propertyName: string): JSONSerializableValue;
-    getProperty(componentName: string, propertyName: string): JSONSerializableValue;
-    getProperty(propertyNameOrComponentName: string, propertyName?: string): JSONSerializableValue {
-        return propertyName ?
-            this.backingObject[propertyNameOrComponentName]?.[propertyName] :
-            this.backingObject[propertyNameOrComponentName];
-    }
+  /**
+   * Gets the value assigned to the specified component and property.
+   * If no component is specified, the default component is assumed.
+   * Returns undefined if the property does not exist.
+   *
+   * @param {string} componentName The name of the component.
+   * @param {string} propertyName  The name of the property.
+   */
+  getProperty(propertyName: string): JSONSerializableValue;
+  getProperty(componentName: string, propertyName: string): JSONSerializableValue;
+  getProperty(propertyNameOrComponentName: string, propertyName?: string): JSONSerializableValue {
+    return propertyName ?
+      this.backingObject[propertyNameOrComponentName]?.[propertyName] :
+      this.backingObject[propertyNameOrComponentName];
+  }
 }
 
 export class ClientProperties {
-    writablePropertiesRequests: ClientPropertyCollection;
-    reportedFromDevice: ClientPropertyCollection;
+  writablePropertiesRequests: ClientPropertyCollection;
+  reportedFromDevice: ClientPropertyCollection;
 
-    constructor(originalTwin?: Twin) {
-        this.writablePropertiesRequests = new ClientPropertyCollection(originalTwin?.properties.desired);
-        this.reportedFromDevice = new ClientPropertyCollection(originalTwin?.properties.reported);
-    }
+  constructor(originalTwin?: Twin) {
+    this.writablePropertiesRequests = new ClientPropertyCollection(originalTwin?.properties.desired);
+    this.reportedFromDevice = new ClientPropertyCollection(originalTwin?.properties.reported);
+  }
 }
