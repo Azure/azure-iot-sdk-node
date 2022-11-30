980f5af75dc112c9504469b22fdaca5c371e792e
anthonyvercolano <toercola@microsoft.com>
2022-11-30 10:22:10 -0800

    chore: update security providers to eslint

--- security/symmetric/test/_symmetric_key_test.js
+++ security/symmetric/test/_symmetric_key_test.js
@@ -2,18 +2,17 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 'use strict';
-var ArgumentError = require('azure-iot-common').errors.ArgumentError;
-var SymmetricKeySecurityClient  = require('../dist/symmetric_key').SymmetricKeySecurityClient ;
-var sinon = require('sinon');
-var assert = require('chai').assert;
+const ArgumentError = require('azure-iot-common').errors.ArgumentError;
+const SymmetricKeySecurityClient  = require('../dist/symmetric_key').SymmetricKeySecurityClient ;
+const assert = require('chai').assert;
 
 describe('symmetric key', function () {
-  describe('getRegistrationId', function() {
-    it('returns the registrationId', function(done) {
-      var fakeRegistrationId = 'registrationId';
-      var fakeSymmetricKey = 'fakeKey';
-      var client = new SymmetricKeySecurityClient(fakeRegistrationId, fakeSymmetricKey);
-      client.getRegistrationId(function(err, returnedId) {
+  describe('getRegistrationId', function () {
+    it('returns the registrationId', function (done) {
+      const fakeRegistrationId = 'registrationId';
+      const fakeSymmetricKey = 'fakeKey';
+      const client = new SymmetricKeySecurityClient(fakeRegistrationId, fakeSymmetricKey);
+      client.getRegistrationId(function (err, returnedId) {
         assert.isNotOk(err);
         assert.equal(returnedId, fakeRegistrationId);
         done();
@@ -21,13 +20,13 @@ describe('symmetric key', function () {
     });
   });
 
-  describe('createSharedAccessSignature', function() {
+  describe('createSharedAccessSignature', function () {
     /*Tests_SRS_NODE_SYMMETRIC_KEY_SECURITY_CLIENT_06_005: [Will throw `ReferenceError` if `idScope` parameter is falsy. ] */
     [undefined, null, ''].forEach(function (badIdScope) {
       it('throws if idScope is \'' + badIdScope +'\'', function () {
-        var fakeRegistrationId = 'registrationId';
-        var fakeSymmetricKey = 'fakeKey';
-        var client = new SymmetricKeySecurityClient(fakeRegistrationId, fakeSymmetricKey);
+        const fakeRegistrationId = 'registrationId';
+        const fakeSymmetricKey = 'fakeKey';
+        const client = new SymmetricKeySecurityClient(fakeRegistrationId, fakeSymmetricKey);
         assert.throws(function () {
           client.createSharedAccessSignature(badIdScope, function () {});
         }, ReferenceError, '');
@@ -35,24 +34,24 @@ describe('symmetric key', function () {
     });
 
     /*Tests_SRS_NODE_SYMMETRIC_KEY_SECURITY_CLIENT_06_006: [The `idScope` parameter must be of type string. ] */
-    [1, true, 15.5, {a: 'b'}].forEach(function (badIdScope) {
+    [1, true, 15.5, { a: 'b' }].forEach(function (badIdScope) {
       it('throws if idScope is \'' + badIdScope +'\'', function () {
-        var fakeRegistrationId = 'registrationId';
-        var fakeSymmetricKey = 'fakeKey';
-        var client = new SymmetricKeySecurityClient(fakeRegistrationId, fakeSymmetricKey);
+        const fakeRegistrationId = 'registrationId';
+        const fakeSymmetricKey = 'fakeKey';
+        const client = new SymmetricKeySecurityClient(fakeRegistrationId, fakeSymmetricKey);
         assert.throws(function () {
           client.createSharedAccessSignature(badIdScope, function () {});
         }, ArgumentError, '');
       });
     });
 
-    it('returns a correct SharedAccessSignature object', function(done) {
+    it('returns a correct SharedAccessSignature object', function (done) {
       /*Tests_SRS_NODE_SYMMETRIC_KEY_SECURITY_CLIENT_06_007: [** A SharedAccessSignature object shall be returned, based on the `idScope`, and the `registrationId` given in the constructor. **] */
-      var fakeRegistrationId = 'registrationId';
-      var fakeSymmetricKey = 'fakeKey';
-      var fakeIdScope = 'fakeScope';
-      var client = new SymmetricKeySecurityClient(fakeRegistrationId, fakeSymmetricKey);
-      client.createSharedAccessSignature(fakeIdScope, function(err, sasTokenObject) {
+      const fakeRegistrationId = 'registrationId';
+      const fakeSymmetricKey = 'fakeKey';
+      const fakeIdScope = 'fakeScope';
+      const client = new SymmetricKeySecurityClient(fakeRegistrationId, fakeSymmetricKey);
+      client.createSharedAccessSignature(fakeIdScope, function (err, sasTokenObject) {
         assert.isNotOk(err);
         assert.equal(sasTokenObject.sr, fakeIdScope + '/registrations/' + fakeRegistrationId);
         assert.equal(sasTokenObject.skn, 'registration');
--- security/tpm/test/_tpm_authentication_provider_test.js
+++ security/tpm/test/_tpm_authentication_provider_test.js
@@ -2,28 +2,23 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 'use strict';
-var errors = require('azure-iot-common').errors;
-var TpmAuthenticationProvider  = require('../dist/tpm_authentication_provider.js').TpmAuthenticationProvider;
-var tss = require("tss.js");
-var sinon = require('sinon');
-var assert = require('chai').assert;
-var crypto = require("crypto");
-var equals = require('array-equal');
+const errors = require('azure-iot-common').errors;
+const TpmAuthenticationProvider  = require('../dist/tpm_authentication_provider.js').TpmAuthenticationProvider;
+const sinon = require('sinon');
+const assert = require('chai').assert;
 
 describe('TpmAuthenticationProvider', function () {
-  var testSecurityClient;
-  var testEK = Buffer.from('EK');
-  var testSRK = Buffer.from('SRK');
-  var testSignedData = Buffer.from('signed data');
-  var testRegistrationId = 'regId';
-  var testCredentials = {
+  let testSecurityClient;
+  const testSRK = Buffer.from('SRK');
+  const testSignedData = Buffer.from('signed data');
+  const testRegistrationId = 'regId';
+  const testCredentials = {
     host: 'test.host',
     deviceId: 'deviceId'
   };
 
   beforeEach(function () {
     testSecurityClient = {
-      getEndorsementKey: sinon.stub().callsArgWith(0, null, testEK),
       getStorageRootKey: sinon.stub().callsArgWith(0, null, testSRK),
       signWithIdentity: sinon.stub().callsArgWith(1, null, testSignedData),
       getEndorsementKey: sinon.stub().callsArg(1),
@@ -36,10 +31,10 @@ describe('TpmAuthenticationProvider', function () {
   });
 
   describe('getDeviceCredentials', function () {
-    /*Tests_SRS_NODE_TPM_AUTH_PROVIDER_16_001: [`getDeviceCredentials` shall use the `SharedAccessSignature.createWithSigningFunction` method with the `signWithIdentity` method of the `TpmSecurityClient` given to the construtor to generate a SAS token.]*/
+    /*Tests_SRS_NODE_TPM_AUTH_PROVIDER_16_001: [`getDeviceCredentials` shall use the `SharedAccessSignature.createWithSigningFunction` method with the `signWithIdentity` method of the `TpmSecurityClient` given to the constructor to generate a SAS token.]*/
     /*Tests_SRS_NODE_TPM_AUTH_PROVIDER_16_002: [`getDeviceCredentials` shall call its callback with an `null` first parameter and the generated SAS token as a second parameter if the SAS token creation is successful.]*/
     it('gets device credentials', function (testCallback) {
-      var authProvider = new TpmAuthenticationProvider(testCredentials, testSecurityClient);
+      const authProvider = new TpmAuthenticationProvider(testCredentials, testSecurityClient);
       authProvider.getDeviceCredentials(function (err, creds) {
         if (err) {
           testCallback(err);
@@ -55,9 +50,9 @@ describe('TpmAuthenticationProvider', function () {
 
     /*Tests_SRS_NODE_TPM_AUTH_PROVIDER_16_003: [`getDeviceCredentials` shall call its callback with an `Error` object if the SAS token creation fails.]*/
     it('calls its callback with an error if signing fails', function (testCallback) {
-      var testError = new Error('test');
+      const testError = new Error('test');
       testSecurityClient.signWithIdentity = sinon.stub().callsArgWith(1, testError);
-      var authProvider = new TpmAuthenticationProvider(testCredentials, testSecurityClient);
+      const authProvider = new TpmAuthenticationProvider(testCredentials, testSecurityClient);
       authProvider.getDeviceCredentials(function (err) {
         assert.strictEqual(err, testError);
         testCallback();
@@ -66,66 +61,66 @@ describe('TpmAuthenticationProvider', function () {
 
     /*Tests_SRS_NODE_TPM_AUTH_PROVIDER_16_004: [`getDeviceCredentials` shall start a timer to renew the SAS token after the time the token is valid minus the renewal margin (60 - 15 = 45 minutes by default).]*/
     it('starts a timer to renew the token', function () {
-      var authProvider = new TpmAuthenticationProvider(testCredentials, testSecurityClient);
+      const authProvider = new TpmAuthenticationProvider(testCredentials, testSecurityClient);
       // configure authProvider to renew the token after 9 seconds.
       authProvider._tokenRenewalMarginInSeconds = 1;
       authProvider._tokenValidTimeInSeconds = 10;
 
-      var newTokenHandler = sinon.stub();
-      this.clock = sinon.useFakeTimers();
+      const newTokenHandler = sinon.stub();
+      const clock = sinon.useFakeTimers();
       authProvider.on('newTokenAvailable', newTokenHandler);
       authProvider.getDeviceCredentials(function () {});
       assert.isTrue(newTokenHandler.calledOnce); // called immediately when the token is first generated.
-      this.clock.tick(8999);
+      clock.tick(8999);
       assert.isTrue(newTokenHandler.calledOnce); // still called once since the token is re-generated when the timer hits 9 seconds.
-      this.clock.tick(1000);
+      clock.tick(1000);
       assert.isTrue(newTokenHandler.calledTwice); // now called twice since 9 seconds have elapsed.
-      this.clock.restore();
+      clock.restore();
       authProvider.stop();
     });
 
     /*Tests_SRS_NODE_TPM_AUTH_PROVIDER_16_008: [a `newTokenAvailable` event shall be emitted if renewing the SAS token succeeds in the timer handler.]*/
     it('timer keeps running to renew the token', function () {
-      var authProvider = new TpmAuthenticationProvider(testCredentials, testSecurityClient);
+      const authProvider = new TpmAuthenticationProvider(testCredentials, testSecurityClient);
       // configure authProvider to renew the token after 9 seconds.
       authProvider._tokenRenewalMarginInSeconds = 1;
       authProvider._tokenValidTimeInSeconds = 10;
 
-      var newTokenHandler = sinon.stub();
-      this.clock = sinon.useFakeTimers();
+      const newTokenHandler = sinon.stub();
+      const clock = sinon.useFakeTimers();
       authProvider.on('newTokenAvailable', newTokenHandler);
       authProvider.getDeviceCredentials(function () {});
       assert.isTrue(newTokenHandler.calledOnce); // called immediately when the token is first generated.
-      this.clock.tick(8999);
+      clock.tick(8999);
       assert.isTrue(newTokenHandler.calledOnce); // still called once since the token is re-generated when the timer hits 9 seconds.
-      this.clock.tick(1000);
+      clock.tick(1000);
       assert.isTrue(newTokenHandler.calledTwice); // now called twice since 9 seconds have elapsed.
-      this.clock.tick(10000);
+      clock.tick(10000);
       assert.isTrue(newTokenHandler.calledThrice);
-      this.clock.restore();
+      clock.restore();
       authProvider.stop();
     });
 
     /*Tests_SRS_NODE_TPM_AUTH_PROVIDER_16_007: [an `error` event shall be emitted if renewing the SAS token fail in the timer handler.]*/
     it('emits an error if signing fails when the token is renewed by the timer', function () {
-      var testError = new Error('test');
-      var authProvider = new TpmAuthenticationProvider(testCredentials, testSecurityClient);
+      const testError = new Error('test');
+      const authProvider = new TpmAuthenticationProvider(testCredentials, testSecurityClient);
       // configure authProvider to renew the token after 9 seconds.
       authProvider._tokenRenewalMarginInSeconds = 1;
       authProvider._tokenValidTimeInSeconds = 10;
-      var errorHandler = sinon.stub();
-      var newTokenHandler = sinon.stub();
+      const errorHandler = sinon.stub();
+      const newTokenHandler = sinon.stub();
       authProvider.on('error', errorHandler);
       authProvider.on('newTokenAvailable', newTokenHandler);
 
-      this.clock = sinon.useFakeTimers();
+      const clock = sinon.useFakeTimers();
       authProvider.getDeviceCredentials(function () {});
       assert.isTrue(newTokenHandler.calledOnce); // called immediately when the token is first generated.
       testSecurityClient.signWithIdentity = sinon.stub().callsArgWith(1, testError);
-      this.clock.tick(9000);
+      clock.tick(9000);
       assert.isTrue(errorHandler.calledOnce);
       assert.isTrue(errorHandler.calledWith(testError));
-      this.clock.restore();
+      clock.restore();
       authProvider.stop();
     });
   });
@@ -133,7 +128,7 @@ describe('TpmAuthenticationProvider', function () {
   describe('updateSharedAccessSignature', function () {
     /*Tests_SRS_NODE_TPM_AUTH_PROVIDER_16_005: [`updateSharedAccessSignature` shall throw an `InvalidOperationError` since it's the role of the TPM to generate SAS tokens.]*/
     it('throws an InvalidOperationError', function () {
-      var authProvider = new TpmAuthenticationProvider(testCredentials, testSecurityClient);
+      const authProvider = new TpmAuthenticationProvider(testCredentials, testSecurityClient);
       assert.throws(function () {
         authProvider.updateSharedAccessSignature('sas');
       }, errors.InvalidOperationError);
@@ -143,31 +138,31 @@ describe('TpmAuthenticationProvider', function () {
   describe('stop', function () {
     /*Tests_SRS_NODE_TPM_AUTH_PROVIDER_16_006: [`stop` shall stop the renewal timer if it is running.]*/
     it('stops the token renewal', function () {
-      var authProvider = new TpmAuthenticationProvider(testCredentials, testSecurityClient);
+      const authProvider = new TpmAuthenticationProvider(testCredentials, testSecurityClient);
       // configure authProvider to renew the token after 9 seconds
       authProvider._tokenRenewalMarginInSeconds = 1;
       authProvider._tokenValidTimeInSeconds = 10;
 
-      var newTokenHandler = sinon.stub();
-      this.clock = sinon.useFakeTimers();
+      const newTokenHandler = sinon.stub();
+      const clock = sinon.useFakeTimers();
       authProvider.on('newTokenAvailable', newTokenHandler);
       authProvider.getDeviceCredentials(function () {});
       assert.isTrue(newTokenHandler.calledOnce); // called immediately when the token is first generated.
-      this.clock.tick(8999);
+      clock.tick(8999);
       assert.isTrue(newTokenHandler.calledOnce); // still called once since the token is re-generated when the timer hits 9 seconds.
-      this.clock.tick(1000);
+      clock.tick(1000);
       assert.isTrue(newTokenHandler.calledTwice); // now called twice since 9 seconds have elapsed.
       authProvider.stop();
-      this.clock.tick(10000)
+      clock.tick(10000)
       assert.isTrue(newTokenHandler.calledTwice); // called twice and not thrice since it is stopped even though 19 seconds have elapsed.
-      this.clock.restore();
+      clock.restore();
     });
   });
 
   describe('fromSecurityClient', function () {
     /*Tests_SRS_NODE_TPM_AUTH_PROVIDER_16_012: [The `fromSecurityClient` method shall instantiate a new `TpmSecurityClient` object.]*/
     it('returns a TpmAuthenticationProvider', function () {
-      var testAuthProvider = TpmAuthenticationProvider.fromTpmSecurityClient(testCredentials.deviceId, testCredentials.host, testSecurityClient);
+      const testAuthProvider = TpmAuthenticationProvider.fromTpmSecurityClient(testCredentials.deviceId, testCredentials.host, testSecurityClient);
       assert.instanceOf(testAuthProvider, TpmAuthenticationProvider);
     });
 
