4d0f62b641714f3ee01c82366e16ef19681425f3
Ubuntu <ewertons@ewertons-ubuntu2204.4zosprmkhvmujeowqqpyvqtuif.xx.internal.cloudapp.net>
2026-01-17 01:32:40 +0000

    Print arguments in deploy.sh

--- node_e2e_resources/deploy.sh
+++ node_e2e_resources/deploy.sh
@@ -24,6 +24,7 @@ location=westus2
 
 while [ "$1" != "" ]; do
     echo "Processing argument '$1'"
+    echo "Args '$1' '$2' '$3' '$4'"
     case $1 in
         -l | --location)
             location=$2
