84e18b051f166b3a0e2c822a92ba7a2cf25e88fc
Tim Taylor <timtay@microsoft.com>
2026-04-16 12:04:04 -0700

    code style fixes

--- device/samples/javascript/tls_13_device.js
+++ device/samples/javascript/tls_13_device.js
@@ -4,15 +4,14 @@
 'use strict';
 
 // TLS 1.3 also works for MQTT over websockets, AMQP, AMQP over websockets, and HTTP
-const ProtocolMqtt = require('azure-iot-device-mqtt').Mqtt;
+const ProtocolAmqp = require('azure-iot-device-amqp').Amqp;
 
 const Client = require('azure-iot-device').Client;
 const Message = require('azure-iot-common').Message;
 
 const deviceConnectionString = process.env.IOTHUB_DEVICE_CONNECTION_STRING;
 
-if (!deviceConnectionString.includes(".device.azure-devices."))
-{
+if (!deviceConnectionString.includes(".device.azure-devices.")) {
     console.error('Device connection string must be in format: \'<hub name>.device.azure-devices.<dnsSuffix>\' for TLS 1.3 support');
     process.exit(-1);
 }
@@ -22,7 +21,7 @@ if (!deviceConnectionString.includes(".device.azure-devices."))
 // the TLS versions used by this SDK for any reason. Additional TLS level settings are described here: https://nodejs.org/api/tls.html
 process.env['NODE_OPTIONS'] = '--tls-min-v1.2 --tls-max-v1.3';
 
-const client = Client.fromConnectionString(deviceConnectionString, ProtocolMqtt);
+const client = Client.fromConnectionString(deviceConnectionString, ProtocolAmqp);
 
 function onConnect(err) {
     if(err) {
@@ -35,7 +34,7 @@ function onConnect(err) {
                 console.log(err.toString());
             } else {
                 console.log('Message sent. Closing connection');
-            };
+            }
 
             console.log('Closing connection');
             client.close();
